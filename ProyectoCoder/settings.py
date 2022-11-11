23af76c0ee18f6599aad8982115822f8f62740a2
Jose Cruz <jose.l.cruz77@gmail.com>
2022-11-11 13:09:21 -0300

    mantenimiento seguridad

--- ProyectoCoder/settings.py
+++ ProyectoCoder/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v72em^5+rzc2gt_f239&9xx498-x88irw0v6)49a+67pnfb$65'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
